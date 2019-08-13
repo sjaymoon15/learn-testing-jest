caa67962dfa715a473fbe7e4516714730d58f86c
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-13 11:14:53 -0500

    Test CommentBox if it has a text area that users can type in

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,14 @@ it('has a text area and a button', () => {
   expect(wrapper.find('textarea').length).toEqual(1);
   expect(wrapper.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+  wrapper
+    .find('textarea')
+    .simulate('change', { target: { value: 'new comment' } });
+
+  // instead of waiting for the rerender to finish
+  // will force the component to update by using update() from enzyme
+  wrapper.update();
+  expect(wrapper.find('textarea').prop('value')).toEqual('new comment');
+});
