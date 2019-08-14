521237aba17ae86c3ef26adc160b5daaac48cfeb
Seungjae Moon <sjaymoon15@gmail.com>
2019-08-14 09:53:09 -0500

    Add a root component to give access to redux store for testing

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
+import Root from 'Root';
 
 let wrapper;
 
 beforeEach(() => {
-  wrapper = mount(<CommentBox />); // using mount for education purpose. shallow should also work fine.
+  wrapper = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  ); // using mount for education purpose. shallow should also work fine.
 });
 
 afterEach(() => {
